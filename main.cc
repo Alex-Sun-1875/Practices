fbd4be6cee63301d63235df69f9ddc80d1fe715e
sunlh <lianghong.sun@seraphic-corp.com>
2018-12-21 18:46:08 +0800

    add sort function to main.cc

--- main.cc
+++ main.cc
@@ -7,21 +7,47 @@
 using std::cin;
 using std::cout;
 
+typedef std::pair<char, int> Pair;
+
+bool compare_for_cout(const Pair& lhs, const Pair& rhs) {
+  if (lhs.second > rhs.second)
+    return lhs.second > rhs.second;
+
+}
+
+bool compare_for_val(const Pair& lhs, const Pair& rhs) {
+  if ((lhs.second == rhs.second) && (lhs.first > rhs.first))
+    return lhs.first > rhs.first;
+}
+
 int main(int argc, char* argv[]) {
   std::string str;
-  std::map<char, int> char_cout_map;
+  std::vector<Pair> pair_vec;
   while (!getline(cin, str)) {
   }
 
   for (auto c : str) {
     if (c != ' ') {
-      if (char_cout_map.end() == (std::map<char, int>::iterator it = char_cout_map.find(c))) {
-        it->first = c;
-        it->second = 1;
+      auto it = std::find_if(pair_vec.begin(), pair_vec.end(), [&](const Pair& item) -> bool { return item.first == c; });
+      if (it != pair_vec.end()) {
+        (*it).second += 1;
       } else {
-        it->second += 1;
+        pair_vec.push_back(Pair(c, 1));
       }
     }
+  }
+
+  for (auto it : pair_vec) {
+    std::cout << it.first << ", " << it.second << std::endl;
+  }
+
+
+  // std::sort(pair_vec.begin(),pair_vec.end(), compare_for_val);
+  std::sort(pair_vec.begin(),pair_vec.end(), compare_for_cout);
+
+  std::cout << std::endl;
+  for (auto it : pair_vec) {
+    std::cout << it.first << ", " << it.second << std::endl;
   }
 
   std::cout << std::endl;
